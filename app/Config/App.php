a73e29c5864f1a0aadc1e134d1f4e5982a143034
exsanputra29-commits <exsanputra29@gmail.com>
2026-04-07 22:12:33 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/gis_faskes_inf24/public/admin';
+    public string $baseURL = 'http://localhost/gis_faskes_inf24/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
