3f763a1b70d0e0aeb333b678579278bff3558bb1
febriands <riandam398@gmail.com>
2026-04-09 11:48:11 +0700

    Update v_home.php

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,48 +1,40 @@
 <div id="map" style="width: 100%; height: 800px;"></div>
 
 <script>
-    var peta1 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11'
-	});
-
-var peta2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/satellite-v9'
-	});
-
-
-var peta3 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	});
-
-var peta4 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/dark-v10'
-	});
-
-    var map = L.map('map').setView([-7.282942510438273, 109.05719608128057], 12);
-
-	var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
-
-    var map = L.map('map', {
-	center: [-7.282942510438273, 109.05719608128057],
-	zoom: 12,
-	layers: [peta1]
+var peta1 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_TOKEN', {
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/streets-v11'
 });
 
-    var baseLayers = {
-	'OpenStreetMap': peta1
-	'OpenStreetMap.HOT': peta2
-    'OpenStreetMap.HOT': peta3
+var peta2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_TOKEN', {
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/satellite-v9'
+});
+
+var peta3 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+});
+
+var peta4 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_TOKEN', {
+    attribution: 'Map data © OpenStreetMap contributors, Imagery © Mapbox',
+    id: 'mapbox/dark-v10'
+});
+
+// Inisialisasi map (CUKUP SEKALI)
+var map = L.map('map', {
+    center: [-7.282942510438273, 109.05719608128057],
+    zoom: 12,
+    layers: [peta1]
+});
+
+// Base layer control
+var baseLayers = {
+    "Streets": peta1,
+    "Satellite": peta2,
+    "OSM": peta3,
+    "Dark": peta4
 };
 
+// Tambahkan kontrol layer
+L.control.layers(baseLayers).addTo(map);
 </script>
